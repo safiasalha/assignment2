c749a44692730f49c825fbc20e6527678f4ebdc1
safiasalha <salhamohdnoh5@gmail.com>
2018-04-08 17:57:00 +0800

    added jserver222.java

---
+++ jserver222.java
@@ -0,0 +1,85 @@
+
+
+import java.io.BufferedReader;
+
+import java.io.InputStreamReader;
+
+import java.net.InetAddress;
+
+import java.net.ServerSocket;
+
+import java.net.Socket;
+
+public class server222 {
+
+    private ServerSocket server;
+
+    public server222(String ipAddress) throws Exception {
+
+        if (ipAddress != null && !ipAddress.isEmpty()) 
+
+          this.server = new ServerSocket(0, 1, 
+InetAddress.getByName(ipAddress));
+
+        else 
+
+          this.server = new ServerSocket(0, 1, 
+InetAddress.getLocalHost());
+
+    }
+
+    private void listen() throws Exception {
+
+        String data = null;
+
+        Socket client = this.server.accept();
+
+        String clientAddress = client.getInetAddress().getHostAddress();
+
+        System.out.println("\r\nNew connection from " + clientAddress);
+
+        
+
+        BufferedReader in = new BufferedReader(
+
+                new InputStreamReader(client.getInputStream()));        
+
+        while ( (data = in.readLine()) != null ) {
+
+            System.out.println("\r\nMessage from " + clientAddress +" " + data);
+
+        }
+
+    }
+
+    public InetAddress getSocketAddress() {
+
+        return this.server.getInetAddress();
+
+    }
+
+    
+
+    public int getPort() {
+
+        return this.server.getLocalPort();
+
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        server222 app = new server222(args[0]);
+
+        System.out.println("\r\nRunning Server: " + 
+
+                "Host=" + app.getSocketAddress().getHostAddress() + 
+
+                " Port=" + app.getPort());
+
+        
+
+        app.listen();
+
+    }
+
+}
